aced148d157b3351c5a7096c5e17f617e236ebf6
Sumit kumar <sk0390777@gmail.com>
2024-04-09 14:06:17 +0530

    Files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import About from './Components/About';
+import React, { useState } from 'react';
+import Alert from './Components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enable or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#042a3a'
+      showAlert(" Dark mode has been enabled", "success")
+      // document.title = 'TextUtils-Dark Modes'
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert(" Light mode has been enabled", "success")
+      // document.title = 'TextUtils-Light Mode'
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route exact path="/about">
+              <About mode={mode}/>
+            </Route>
+            <Route exact path="/">
+              <TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Chartacter Counter " mode={mode} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
-
 export default App;
